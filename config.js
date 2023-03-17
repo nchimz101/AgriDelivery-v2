2ae19a93b5bfa83b19270c7bf3970660841efc54
Nchimz101 <munyama01@gmail.com>
2023-03-17 09:11:02 +0200

    commit

--- config.js
+++ config.js
@@ -1,15 +1,14 @@
 //Main data
 exports.domain="https://netagrow.cloud";
-exports.APP_NAME="Netagrow for Vendors";
-exports.LOGO="https://foodtiger.site/default/logo.png";
-exports.LOGOWidth=487;
-exports.LOGOHeight=144;
+exports.APP_NAME="Netagrow For Vendors";
+exports.LOGO="https://www.netagrow.cloud/uploads/settings/d1e1bbaf-6a44-49b2-974e-e85272e10f8f_logo.jpg";
+exports.LOGOWidth=250;
+exports.LOGOHeight=250;
 
 //Currency
 exports.currency="USD";
 exports.currencySign="$";
 exports.enableCOD=true;  //Cash on deliver
-
 
 //Date format
 exports.dateTimeFormat="MMMM Do YYYY, h:mm a";
@@ -17,7 +16,7 @@ exports.dateFormat="MMMM Do YYYY";
 
 //Registering
 exports.disableRegister=false; //Set to true if you want to remove the option to register from mobile app
-exports.APP_SECRET="app_secret";
+exports.APP_SECRET="Munya@101";
 
 
 //Optional
@@ -28,7 +27,7 @@ exports.SINGLE_MODE_NAME="Restaurant Name";
 exports.MULTI_CITY=true;
 
 //Google setup
-exports.GOOGLE_API_KEY="YOUR-GOOGLE_API_KEY";
+exports.GOOGLE_API_KEY="GOOGLE_API_KEY";
 exports.queryTypes="address"
 exports.queryCountries=['us']; //{['pl', 'fr']}
 exports.searchLatitude=null; //{500}
